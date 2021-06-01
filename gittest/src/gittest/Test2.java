e19a396feeb33f983b7f49b84499291897a31600
luguanmin <453222744@qq.com>
2021-06-01 09:55:35 +0800

    20210601commit

--- gittest/src/gittest/Test2.java
+++ gittest/src/gittest/Test2.java
@@ -1,24 +1,5 @@
 package gittest;
 
 public class Test2 {
-	//Test2 master 
-	//Test2 dev 
-	//拉取dev 后 maste 开发完成
-	//从master拉取代码 ,dev开发完成
-	//组长开发代码
-	
-	//功能完成,提交
-	
-	//新功能公用代码
-	
-	//新功能17:25
-
-
-	
-	//合并其他代码了
-	//巴拉巴拉开发
-
-//github直接修改
-	
-	//master上修改了
+	//shanchu
 }
