ccf28a3792087a7ff8672f38e4075e1957eb78a6
luguanmin <453222744@qq.com>
2021-06-01 10:42:37 +0800

    hebing master

--- gittest/src/gittest/Test2.java
+++ gittest/src/gittest/Test2.java
@@ -1,34 +1,9 @@
 package gittest;
 
 public class Test2 {
-	//Test2 master 
-	//Test2 dev 
-	//拉取dev 后 maste 开发完成
-	//从master拉取代码 ,dev开发完成
-	//组长开发代码
-	
-	//功能完成,提交
-	
-	//新功能公用代码
-	
-	//新功能17:25
+	//hebing master
 
 
-	
-	//合并其他代码了
-	//巴拉巴拉开发
-
-//github直接修改
-	
-	//master上修改了
-
-
-	//dev修改了
-	
-	//453在 自己的dev分支开发完成提交  1111
-	//453 dev开发完成
-	
-	//同事提交代码.已合并  源dev合并了新的2020/04/28 19:26
 	
 
 }
