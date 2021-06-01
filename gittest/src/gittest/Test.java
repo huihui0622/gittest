87b12db137f554a9141f6b7945fd42d6f83b9ac6
luguanmin <453222744@qq.com>
2021-06-01 11:17:22 +0800

    fenzhi0601 提交

--- gittest/src/gittest/Test.java
+++ gittest/src/gittest/Test.java
@@ -2,15 +2,5 @@ package gittest;
 
 public class Test {
 		//1 初始化
-	//dev 开发1
-	//dev2 修改编码
-	//回退到未修改编码的状态 history -reset
-	//再次提交
-	//再次提交2222
-	
-	//dev分支修改1
-	//master修改了
-	//dev修改2
 
-    //20210525提交222333555
 }
